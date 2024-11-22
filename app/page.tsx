fd590a5fa891618fd99982ac7321bcacdc93a2f4
facu <facundogregori@hotmail.com>
2024-11-22 18:39:34 -0300

    fix build

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,12 @@
 import { useState } from 'react'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import { PurchaseForm } from '@/components/purchase-form'
-import BuyModal from '@/components/moon-pay/moon-pay-buy-modal'
-import dynamic from 'next/dynamic'
-// import { MoonPayProvider } from '@moonpay/moonpay-react';
 
-const MoonPayProvider = dynamic(
-  () => import('@moonpay/moonpay-react').then((mod) => mod.MoonPayProvider),
-  { ssr: false }
-)
 export default function Home() {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [activeTab, setActiveTab] = useState<'fiat' | 'crypto'>('fiat')
 
   return (
-    // <MoonPayProvider apiKey="pk_test_YHaGUnoZCiKe1wPcyl26uZ89LDOOsRG" debug>
     <div className="max-w-md mx-auto p-10 ">
       <div className="max-w-md mx-auto mb-4">
         <h1 className="text-2xl font-bold mb-4">Buy Fiat or Crypto</h1>
@@ -42,9 +35,6 @@ export default function Home() {
           </TabsContent>
         </Tabs>
       </div>
-      {/* <BuyModal /> */}
     </div>
-
-    // </MoonPayProvider>
   )
 }
--- app/pocs/page.tsx
+++ app/pocs/page.tsx
@@ -1,23 +1,17 @@
 'use client'
 
-import { useState } from 'react'
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
-import { PurchaseForm } from '@/components/purchase-form'
-import BuyModal from '@/components/moon-pay/moon-pay-buy-modal'
+import  MoonPayBuyModal  from '@/components/moon-pay/moon-pay-buy-modal'
 import dynamic from 'next/dynamic'
-// import { MoonPayProvider } from '@moonpay/moonpay-react';
 
 const MoonPayProvider = dynamic(
   () => import('@moonpay/moonpay-react').then((mod) => mod.MoonPayProvider),
   { ssr: false }
 )
 export default function Home() {
-  // const [activeTab, setActiveTab] = useState<"fiat" | "crypto">("fiat")
-
   return (
     <MoonPayProvider apiKey="pk_test_YHaGUnoZCiKe1wPcyl26uZ89LDOOsRG" debug>
       <div className="max-w-md mx-auto p-10 ">
-        <BuyModal />
+        <MoonPayBuyModal />
       </div>
     </MoonPayProvider>
   )
